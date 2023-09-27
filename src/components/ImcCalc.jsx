07c9af8b8c029871fdec9789c04de3ebc7269b4d
Adriano Pessoa <adrianobpessoa@gmail.com>
2023-09-27 03:48:10 -0300

    feat: insert style in calculator

--- src/components/ImcCalc.jsx
+++ src/components/ImcCalc.jsx
@@ -1,3 +1,4 @@
+import Button from './Button';
 import './ImcCalc.css';
 
 const ImcCalc = () => {
@@ -16,8 +17,8 @@ const ImcCalc = () => {
                 </div>
             </div>
             <div className="action-control">
-                <button>Calcular</button>
-                <button>Limpar</button>
+                <Button id="calc-btn" text="Calcular"/>
+                <Button id="clear-btn" text="Limpar"/>
             </div>
         </form>
     </div>
